75ad43e15b5d6d64fadf22d9223ffa885af5ffc1
unknown <sanchit2005yudavanshi@gmail.com>
2023-08-15 16:09:44 +0530

    adde seach icon gif and a static image

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,6 @@ function Home({ data }) {
               onChange={handleChange}
               value={value}
             />
-            
           </form>
         </nav>
         <motion.div className={styles.card}>
