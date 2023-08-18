f43b9e2b8339736ba7e11c90127e40bd3810de2d
unknown <sanchit2005yudavanshi@gmail.com>
2023-08-18 22:08:15 +0530

    working fine

--- weather_forecast/src/Main.js
+++ weather_forecast/src/Main.js
@@ -1,19 +1,36 @@
 import axios from "axios";
 import useInputHook from "./hooks/useInputHook";
+import "./main.css";
+import { useState } from "react";
 
 function Main() {
+  const [data, setData] = useState("");
   const [value, handleChange, reset] = useInputHook("");
+  async function getData() {
+    const res = await axios.get(
+      `https://api.weatherapi.com/v1/current.json?key=67ca48d558fa4143a9600726230708&q=${value}&aqi=no`
+    );
+    const { data } = res;
+    setData(data);
+    // console.log(data.location.name)
+  }
   return (
     <>
       <form
         onSubmit={(event) => {
           event.preventDefault();
           reset();
+          getData();
         }}
       >
-        <input value={value} onChange={handleChange} type="text" />
+        <input
+          value={value}
+          onChange={handleChange}
+          type="text"
+          className="search"
+        />
       </form>
-      <h1>{value}</h1>
+      <h1>{}</h1>
     </>
   );
 }
