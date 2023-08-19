6a47ce376e6894f1faf3529d69d5a20757eb2498
unknown <sanchit2005yudavanshi@gmail.com>
2023-08-19 23:47:44 +0530

    geocoding api is working

--- weather_forecast/src/Main.js
+++ weather_forecast/src/Main.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 import useInputHook from "./hooks/useInputHook";
 import "./main.css";
-import { useState } from "react";
+import { useState} from "react";
 
 function Main() {
-  const [data, setData] = useState("");
-  const [value, handleChange, reset] = useInputHook("");
-  async function getData() {
-    const res = await axios.get(
-      `https://api.weatherapi.com/v1/current.json?key=67ca48d558fa4143a9600726230708&q=${value}&aqi=no`
-    );
-    const { data } = res;
-    setData(data);
-    // console.log(data.location.name)
+  const[query, setQuery] = useState([]);
+  const[value,handleChange,reset] = useInputHook("");
+  const apiKey = "4990e2be0dec0c8e71f7e52dd733e707"
+  const getData = async () =>{
+    const res = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${value}&appid=${apiKey}`);
+    setQuery(res.data);
+    const a = query.map(p=>(
+      console.log(p.lat)
+    ))
+    console.log(a)
   }
   return (
     <>
@@ -28,6 +29,7 @@ function Main() {
           onChange={handleChange}
           type="text"
           className="search"
+          placeholder="Search"
         />
       </form>
       <h1>{}</h1>
