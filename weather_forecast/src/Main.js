68510597579899cc7127a3a8b1c21810f0ab5199
unknown <sanchit2005yudavanshi@gmail.com>
2023-08-20 16:21:38 +0530

    just displaying the data is left

--- weather_forecast/src/Main.js
+++ weather_forecast/src/Main.js
@@ -4,11 +4,14 @@ import "./main.css";
 import { useState} from "react";
 
 function Main() {
+
   const[lat, setLat] = useState([]);
   const[long, setLong] = useState([]);
   const[query, setQuery] = useState([]);
+  const[weatherData, setWeatherData] = useState([]);
   const[value,handleChange,reset] = useInputHook("");
   const apiKey = "7b38de6a6fa08f51ab08ef36085004d5"
+
   const getData = async () =>{
     const res = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${value}&appid=${apiKey}`);
     setQuery(res.data);
@@ -18,13 +21,12 @@ function Main() {
     query.map(p => (
       setLong(p.lon)
     ))
-    console.log(lat)
-    console.log(long)
+    getTemp();
   }
 
   const getTemp = async () => {
-    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`);
-    console.log(res.data);
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`);
+    console.log(res.data.main.temp)
   }
   return (
     <>
@@ -32,8 +34,6 @@ function Main() {
         onSubmit={(event) => {
           event.preventDefault();
           reset();
-          getData()
-          getTemp();
         }}
       >
         <input
@@ -43,8 +43,11 @@ function Main() {
           className="search"
           placeholder="Search"
         />
+        <button onClick={getData}>Click</button>
       </form>
-      <h1>{}</h1>
+      {/* <h1>{weatherData.map(data => (
+        <p>{data.main.temp}</p>
+      ))}</h1> */}
     </>
   );
 }
