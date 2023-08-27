d78147d18f331b5b9350e96ff7a0bc0c6b8c5e04
unknown <sanchit2005yudavanshi@gmail.com>
2023-08-27 14:07:40 +0530

    installed dotEnv to protect our api key

--- weather_forecast/src/Main.js
+++ weather_forecast/src/Main.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import useInputHook from "./hooks/useInputHook";
 import "./main.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Weather from "./Weather";
 
 const Main = () => {
+  
   const [data, setData] = useState([]);
   const [value, handleChange, reset] = useInputHook("");
   const apiKey = "67ca48d558fa4143a9600726230708"
@@ -12,7 +13,6 @@ const Main = () => {
   const handleClick = async () => {
     const res = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${value}&aqi=no`)
    setData(res.data)
-   console.log(data)
   };
   if(data){
     return(
